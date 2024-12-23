27f5257fe7b19e7c0919372e27a75c21d51b30b0
Prktfnt <67832918+Prktfnt@users.noreply.github.com>
2024-12-23 23:12:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Liste mit Hinweisen
+const hints = [
+  "Falsches Passwort, versuche es erneut!",
+  "Tipp: Ich hab mich an deiner Passwort auswahl inspirieren lassen",
+  "Tipp: weihnachten",
+  "Tipp: x.weihnachtenxx.x",
+  "Tipp: Es beginnt mit einem 'T' und endet mit 'B'.",
+  "Das Passwort ist T.weihnachten24.B",
+];
+
+// Zähler für die Hinweise
+let hintIndex = 0;
+
 function checkPassword() {
   const correctPassword = "T.weihnachten24.B";
   const input = document.getElementById("password").value;
@@ -8,7 +21,10 @@ function checkPassword() {
     result.textContent = "";
     surprise.classList.remove("hidden");
   } else {
-    result.textContent = "Falsches Passwort. Versuch es noch einmal!";
-    result.style.color = "red";
+    result.textContent = hints[hintIndex];
+    result.style.color = "orange";
+
+    // Erhöhe den Index, starte bei 0, wenn alle Hinweise gezeigt wurden
+    hintIndex = (hintIndex + 1) % hints.length;
   }
 }
